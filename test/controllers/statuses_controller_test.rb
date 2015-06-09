af37586a1cab77fdffce3b1caa3eb805c65c1bbc
Dustin Milam <darklordsushi@yahoo.com>
2015-06-09 00:16:43 -0400

    added auth to status controller and tests, and status validations and tests

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -11,7 +11,7 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should be redirected when not loged in" do
+  test "should be redirected when not logged in" do
     get :new
     assert_response :redirect
     assert_redirected_to new_user_session_path
@@ -23,7 +23,15 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should be logged in to post a status" do 
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should create status when logged in" do
+    sign_in users(:dustin)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -36,17 +44,41 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should be logged in get edit" do 
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit when logged in" do
+    sign_in users(:dustin)
+
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should be logged in to update a status" do 
+    patch :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:dustin)
+
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should be logged in to delete a status" do 
+    delete :destroy, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should destroy status" do
+    sign_in users(:dustin)
+
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
